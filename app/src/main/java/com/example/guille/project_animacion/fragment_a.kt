aab2704bc66d43c5d6e730900a1f773ea1abbfbe
Guillermo Garcia <guigarmart@gmail.com>
2018-03-17 18:38:40 +0100

    Fragments

---
+++ app/src/main/java/com/example/guille/project_animacion/fragment_a.kt
@@ -0,0 +1,14 @@
+package com.example.guille.project_animacion
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+/**
+ * Created by Guille on 17/03/2018.
+ */
+class fragment_a : Fragment() {
+
+
+}
